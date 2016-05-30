65b31de8b7c55350d52f7c9c63e57c60b281cceb
duniv5-007 <duniv5-007@duniv5-007-PC>
2016-05-30 14:02:18 +0900

    클래스 수정함

--- WhenWhere/src/main/java/com/whenwhere/board/cont/BoardController.java
+++ WhenWhere/src/main/java/com/whenwhere/board/cont/BoardController.java
@@ -1,5 +1,14 @@
 package com.whenwhere.board.cont;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
 public class BoardController {
 
+	@RequestMapping(value = "/notice")
+	public String notice() {
+		return "board/noticeBoard";
+	}
 }
