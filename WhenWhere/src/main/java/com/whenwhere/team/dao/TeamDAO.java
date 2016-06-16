9301d96c84bd783f7616106a9f14ae73530b83dc
yeonsoo <itam6849@naver.com>
2016-06-16 23:48:12 +0900

    TeamDAO

--- WhenWhere/src/main/java/com/whenwhere/team/dao/TeamDAO.java
+++ WhenWhere/src/main/java/com/whenwhere/team/dao/TeamDAO.java
@@ -13,4 +13,5 @@ public interface TeamDAO {
 	public int makeTeam(Map<String, Object> map);
 	public List<TeamVO> getTeamList(String nickName);
 	public List<String> getTeamUserList(@Param("nickname") String nickname, @Param("teamNum") int teamNum);
+
 }
